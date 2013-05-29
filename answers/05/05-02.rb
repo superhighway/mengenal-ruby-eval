184d7bdca3820d1799d5ac840180ecc456cd1a01
Cat Cyborg <thecatcyborg@gmail.com>
2013-05-29 17:58:30 +0800

    tambahkan newline ke jawaban...

--- answers/05/05-02.rb
+++ answers/05/05-02.rb
@@ -1,7 +1,6 @@
-result = <<-EOT
+File.read("/Home/komik.txt") + "\n" == <<-EOT
 PHD Comics: http://www.phdcomics.com/comics.php
 Dilbert: http://dilbert.com
 Chicken Strip: http://chickenstrip.wordpress.com/
 XKCD: http://xkcd.com/
 EOT
-File.read("/Home/komik.txt") == result.strip
