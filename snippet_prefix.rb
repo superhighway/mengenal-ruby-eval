99ddc588f5e1b57767ea0a7b6af862e60cd0a19a
Cat Cyborg <thecatcyborg@gmail.com>
2013-05-29 18:21:12 +0800

    patch File#foreach

--- snippet_prefix.rb
+++ snippet_prefix.rb
@@ -41,6 +41,11 @@ else
   require 'fakefs'
 end
 
+class File
+  def self.foreach(path)
+    self.read(path).each_line { |line| yield(line) }
+  end
+end
 
 class << IO
   [:sysopen, :for_fd, :popen, :readlines, :read, :binread, :binwrite, :select, :pipe, :try_convert, :copy_stream, :remove_class_variable, :class_variable_get, :class_variable_set, :public_constant, :private_constant, :public_instance_method, :!~, :singleton_class, :untrust, :untrusted?, :trust, :remove_instance_variable, :tap, :public_send, :extend, :display, :method, :public_method, :define_singleton_method, :object_id, :to_enum, :enum_for, :__id__].each { |m| define_method( m) { |*args| undef_method m }}
