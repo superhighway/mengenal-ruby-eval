87297366eee6782f432a9c9839a0df0e512fcb8a
Cat Cyborg <thecatcyborg@gmail.com>
2013-05-29 17:44:11 +0800

    strip string multiline

--- answers/05/05-02.rb
+++ answers/05/05-02.rb
@@ -1,6 +1,7 @@
-File.read("/Home/komik.txt") == <<-EOT
+result = <<-EOT
 PHD Comics: http://www.phdcomics.com/comics.php
 Dilbert: http://dilbert.com
 Chicken Strip: http://chickenstrip.wordpress.com/
 XKCD: http://xkcd.com/
 EOT
+File.read("/Home/komik.txt") == result.strip
